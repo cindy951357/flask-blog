98ebca1a06b924cae7269099edf27a3c49945037
cindy951357 <cindy951357@gmail.com>
2025-01-24 11:20:05 +0800

    added - env files settings

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
 
 db = SQLAlchemy()
 migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://cindy:cindymysql2@localhost/blog_db'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://cindy:cindymysql2@localhost/blog_db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = (
+        f"mysql+pymysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}"
+        f"@{os.getenv('MYSQL_HOST')}:{os.getenv('MYSQL_PORT')}/{os.getenv('MYSQL_DB')}"
+    )
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
